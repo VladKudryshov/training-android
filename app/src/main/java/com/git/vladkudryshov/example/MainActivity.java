dcde455b8ef9ad819bcd8069178ad7e4ee5a4b6c
VladKudryshov <kudryshov_vv_15@mail.ru>
2017-10-04 17:00:43 +0300

    add log to level debug

--- app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
+++ app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
@@ -2,12 +2,15 @@ package com.git.vladkudryshov.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "The some text");
     }
 }
