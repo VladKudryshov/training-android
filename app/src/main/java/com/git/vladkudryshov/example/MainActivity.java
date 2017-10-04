d93117c691ab3bf0c62c689ed3be033bfcbbfb8a
VladKudryshov <kudryshov_vv_15@mail.ru>
2017-10-04 18:11:45 +0300

    cherry pic

--- app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
+++ app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
@@ -2,12 +2,16 @@ package com.git.vladkudryshov.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String Tag = "MainActivity";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(Tag, "Cherry pic");
     }
 }
