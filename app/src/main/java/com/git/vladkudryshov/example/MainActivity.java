2c14e5d593cc69a3c69f978b8d7a05d9822db04a
VladKudryshov <kudryshov_vv_15@mail.ru>
2017-10-04 17:53:31 +0300

    delete log to level debug

--- app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
+++ app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
@@ -1,18 +1,13 @@
 package com.git.vladkudryshov.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String TAG = "MainActivity";
-
+    
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "The some text");
-        Log.d(TAG, "Some text feature");
     }
 }
