16709e05048a3dc59ad26ae8830008382f76a205
VladKudryshov <kudryshov_vv_15@mail.ru>
2017-10-04 18:17:07 +0300

    add some text for log to level debug

--- app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
+++ app/src/main/java/com/git/vladkudryshov/example/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(Tag, "Cherry pic");
+        Log.d(Tag, "Some text");
     }
 }
